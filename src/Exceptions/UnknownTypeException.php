b23957898d567b2e0c0112bac2fd0b780d7fdbb4
Tommy Stigen Olsen <tommysolsen@gmail.com>
2022-03-09 17:49:04 +0100

    Fix available types looking failing to show registered names, but class names instead.

--- src/Exceptions/UnknownTypeException.php
+++ src/Exceptions/UnknownTypeException.php
@@ -12,7 +12,8 @@ class UnknownTypeException extends FormBuilderException
     public function __construct(string $wrongType, Throwable $previous = null)
     {
         $container = Container::getInstance();
-        $types = collect($container->get(FormFieldRepository::class))->keys()->join(",") ?: "no valid types";
-        parent::__construct("Unknown type $wrongType, valid types are: $types", 100, $previous);
+        $types = $container->get(FormFieldRepository::class)->getSupportedFields();
+        $types = collect($types)->join(", ") ?: "no valid types";
+        parent::__construct("Unknown type $wrongType, valid types are: $types. Have you forgotten to register it?", 100, $previous);
     }
 }
